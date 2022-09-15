e50211158551e16f7d660b64d01f73534391f453
Efe Oral <efeoral@gmail.com>
2022-09-15 21:16:05 +0300

    battery pickup added

--- Assets/Script/BatteryPickup.cs
+++ Assets/Script/BatteryPickup.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BatteryPickup : MonoBehaviour
+public class Battery : MonoBehaviour
 {
     [SerializeField] float restoreAngle;
     [SerializeField] float restoreIntensity;
